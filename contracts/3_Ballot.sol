e0da765c02d829d492d6afc8620c913a07f77c86
"Roger" <"roger.rangel.ee@gmail.com">
2022-05-02 11:28:31 +0200

    Contributing to the Campaign

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -5,9 +5,16 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Campaign {
     address public manager;
     uint public minimumContribution;
+    address[] public approvers;
 
     constructor(uint minimum) {
         manager = msg.sender;
         minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+
+        approvers.push(msg.sender);
     }
 }
