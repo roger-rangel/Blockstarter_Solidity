0c84baeb13c7c203cc9f44e8c1673dbec0b29352
"Roger" <"roger.rangel.ee@gmail.com">
2022-05-02 15:54:49 +0200

    Creating a Request

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -3,9 +3,23 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
+    Request[] public requests;
+
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
 
     constructor(uint minimum) {
         manager = msg.sender;
@@ -16,5 +30,17 @@ contract Campaign {
         require(msg.value > minimumContribution);
 
         approvers.push(msg.sender);
+    }
+
+    function createRequest(string memory description, uint value, address recipient) public restricted {
+            Request memory newRequest = Request({
+                description: description,
+                value: value,
+                recipient: recipient,
+                complete: false
+            });
+
+
+            requests.push(newRequest);
     }
 }
